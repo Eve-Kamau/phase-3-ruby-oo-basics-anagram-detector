bbba9b72845e25f954c21453754f9e22e9a4e452
Eve-Kamau <evelyne.kamau@student.moringaschool.com>
2023-03-27 02:51:46 +0300

    commit tree c6e5f589cd5c0614e985e852d2d5073285ab9d13

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+
+  def initialize(anagram)
+      @anagram = anagram
+  end
+
+  def is_anagram?(text)
+    text.chars.sort == @anagram.chars.sort
+  end
+
+  def match(words)
+    words.filter do |text|
+      is_anagram?(text)
+    end
+  end
+
+end
+
+listen = Anagram.new("listen")
+listen.match(%w[enlists google inlets banana])
